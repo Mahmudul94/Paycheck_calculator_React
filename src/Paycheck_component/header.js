14c0813156e7dd2f31b0e97faff8c640986d3e34
Mahmudul94 <mahalam94@gmail.com>
2020-05-15 03:18:06 -0400

    Added option and Nativeselect

--- src/Paycheck_component/header.js
+++ src/Paycheck_component/header.js
@@ -51,25 +51,23 @@ export default class TopShelf extends Component {
                         General Information
           </Box>
                 </div>
-                <div className="gross">
+                <div >
                     <FormControl>
                         <div>
                             <strong > <a href="#">Gross Pay </a> </strong>
-
-                            <TextField id="outlined-basic" label="" variant="outlined" />
+                            <input className="gross_textfield" type="text"></input>
                         </div>
                     </FormControl>
-                    <Select>
-                        <MenuItem value="">Annualy</MenuItem>
-                        <MenuItem>Pay Per Period</MenuItem>
-                    </Select>
+                    <NativeSelect className="gross_textfield">
+                        < option value=""> Annualy</option>
+                        < option>Pay Per Period </option>
+                    </NativeSelect>
                 </div>
-                <div>
-                    <strong>
-                        {" "}
+                <div >
+                    <strong >
                         <a href=" ">Gross Salary YTD</a>
                     </strong>
-                    <TextField id="outlined-basic" label="" variant="outlined" />
+                    <input type="text"></input>
           (optional)
         </div>
                 <div>
@@ -77,49 +75,48 @@ export default class TopShelf extends Component {
                         {" "}
                         <a href=" ">Pay Frequency</a>
                     </strong>
-                    <Select>
-                        <MenuItem value="">Daily</MenuItem>
-                        <MenuItem>Weekly</MenuItem>
-                        <MenuItem>Bi-Weekly</MenuItem>
-                        <MenuItem>Semi-Monthly</MenuItem>
-                        <MenuItem>Monthly</MenuItem>
-                        <MenuItem>Quarterly</MenuItem>
-                        <MenuItem>Semi-Annual</MenuItem>
-                        <MenuItem>Annual</MenuItem>
-                    </Select>
+                    <NativeSelect>
+                        <option value="">Daily</option>
+                        <option>Weekly</option>
+                        <option>Bi-Weekly</option>
+                        <option>Semi-Monthly</option>
+                        <option>Monthly</option>
+                        <option>Quarterly</option>
+                        <option>Semi-Annual</option>
+                        <option>Annual</option>
+                    </NativeSelect>
                 </div>
 
                 <div>
                     <strong>
-                        {" "}
+
                         <a href=" ">Federal Filing Status</a>
                     </strong>
-                    <Select>
-                        <MenuItem value="">Single</MenuItem>
-                        <MenuItem value="">Married</MenuItem>
-                        <MenuItem value="">Head of Household</MenuItem>
-                        <MenuItem value="">
-                            Married But Withhold at the Higher Single Rate
-            </MenuItem>
-                    </Select>
+                    <NativeSelect>
+                        <option >Single</option>
+                        <option >Married</option>
+                        <option>Head of Household</option>
+                        <option >
+                            Married But Withhold at the Higher Single Rate </option>
+                    </NativeSelect>
                 </div>
                 <div>
                     <strong>
-                        {" "}
+
                         <a href=" "># of Fedaral Allowances</a>
                     </strong>
-                    <TextField id="outlined-basic" label="" variant="outlined" />
+                    <input type="text"></input>
                 </div>
                 <div>
                     <strong>
-                        {" "}
+
                         <a href=" ">Additional Federal Withholding</a>
                     </strong>
-          $ <TextField id="outlined-basic" label="" variant="outlined" />
+                    $<input type="text"></input>
                 </div>
                 <div>
                     <strong>
-                        {" "}
+
                         <a href=" ">Round Federal Withholding</a>
                     </strong>
                     <Radio value="Y" name="radio-button-demo" /> Yes
@@ -127,7 +124,7 @@ export default class TopShelf extends Component {
         </div>
                 <div>
                     <strong>
-                        {" "}
+
                         <a href=" ">I am exempt from</a>
                     </strong>
                     <Checkbox name="" />
@@ -147,28 +144,27 @@ export default class TopShelf extends Component {
                         {" "}
                         <a href=" "> Filing Status</a>
                     </strong>
-                    <Select>
-                        <MenuItem value="">Single</MenuItem>
-                        <MenuItem value="">Married</MenuItem>
-                        <MenuItem value="">Head of Household</MenuItem>
-                        <MenuItem value="">
+                    <NativeSelect>
+                        <option>Single</option>
+                        <option>Married</option>
+                        <option>Head of Household</option>
+                        <option>
                             Married But Withhold at the Higher Single Rate
-            </MenuItem>
-                    </Select>
+            </option>
+                    </NativeSelect>
                 </div>
                 <div>
                     <strong>
-                        {" "}
                         <a href=" ">Allowances</a>
                     </strong>
-                    <TextField id="outlined-basic" label="" variant="outlined" />
+                    <input type="text"></input>
                 </div>
                 <div>
                     <strong>
                         {" "}
                         <a href=" ">Additional State W/H</a>
                     </strong>
-          $ <TextField id="outlined-basic" label="" variant="outlined" />
+                    $<input type="text"></input>
                 </div>
                 <div>
                     <strong>
@@ -183,19 +179,19 @@ export default class TopShelf extends Component {
                         {" "}
                         <a href=" "> City Tax</a>
                     </strong>
-                    <Select>
-                        <MenuItem value="">None</MenuItem>
-                        <MenuItem value=""></MenuItem>
-                        <MenuItem value=""></MenuItem>
-                        <MenuItem value=""></MenuItem>
-                    </Select>
+                    <NativeSelect>
+                        <option>None</option>
+                        <option></option>
+                        <option></option>
+                        <option></option>
+                    </NativeSelect>
                 </div>
                 <div>
                     <strong>
-                        {" "}
+
                         <a href=" ">NYC Allowances</a>
                     </strong>
-                    <TextField id="outlined-basic" label="" variant="outlined" />
+                    <input type="text"></input>
                 </div>
             </div>
         );
